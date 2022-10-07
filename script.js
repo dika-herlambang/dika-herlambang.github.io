972ad557fc85c67002140519853be7fe2ea3daae
dika-herlambang <dikaprogramming@gmail.com>
2022-10-07 15:25:34 +0700

    create new project

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('fa-times');
+	navbar.classList.toggle('active');
+}
+
+window.onscroll = ()=>{
+	menu.classList.remove('fa-times');
+	navbar.classList.remove('active');
+
+	if (window.scrollY > 60) {
+		document.querySelector('#scroll-top').classList.add('active');
+	} else {
+		document.querySelector('#scroll-top').classList.remove('active');
+	}
+}
+
+  var typed = new Typed (".typing", {
+    strings: ["DIKA HERLAMBANG", "FRONT END DEVELOPER" ,"WEB DESIGNER"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
+});
+
